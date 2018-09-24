52951fb4b502283de9e203604b464da65bbe14fc
mabramovsd <beavisabra@yandex.ru>
2018-09-24 12:43:57 +0400

    Удаление картинок, отображение меню

--- Lib/MENU.cpp
+++ Lib/MENU.cpp
@@ -1,12 +1,48 @@
 #include "TXLib.h"
-#include "CheckFocus.cpp"
-void menu () 
-{
-    int screenW = GetSystemMetrics (SM_CXSCREEN);
-    int screenH = GetSystemMetrics (SM_CXSCREEN);
 
-    HDC fon_menu = txLoadImage ("Pics\\fon_menu.bmp");
+bool checkFocus(int MinX, int MaxX, int MinY, int MaxY);
+void checkMenuFocus();
+void drawMenu (int screenW, int screenH, HDC fon_menu);
+
+bool checkFocus(int MinX, int MaxX, int MinY, int MaxY)
+{
+    if (
+        txMouseX() >= MinX && //  мышь находится на кнопке
+        txMouseX() <= MaxX &&
+        txMouseY() <= MaxY &&
+        txMouseY() >= MinY
+        )
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void checkMenuFocus()
+{
+    if (checkFocus(40, 560, 810, 850))
+    {
+        txTextOut(txMouseX(), txMouseY() - 20, "Запускает программу" );
+    }
+
+    else if (checkFocus(40, 560, 880, 920))
+    {
+        txTextOut(txMouseX(), txMouseY() - 20, "Сохранение чего-то" );
+    }
+
+    else if (checkFocus(40, 560, 940, 980))
+    {
+        txTextOut(txMouseX(), txMouseY() - 20, "Загрузка выбранной планировки" );
+    }
+
+    else if (checkFocus(40, 560, 1000, 1040))
+    {
+        txTextOut(txMouseX(), txMouseY() - 20, "Как ни странно, но это настройки" );
+    }
+}
+
+void drawMenu (int screenW, int screenH, HDC fon_menu)
+{
     txBitBlt (txDC(), 0, 0, screenW, screenH, fon_menu, 0, 0);
-    txDeleteDC(fon_menu);
-    CheckMenuFocus();
 }
--- main.cpp
+++ main.cpp
@@ -2,14 +2,23 @@
 #include "Lib\\MENU.cpp"
 
 int main()
-    {
+{
     int screenW = GetSystemMetrics (SM_CXSCREEN);
     int screenH = GetSystemMetrics (SM_CXSCREEN);
-
     txCreateWindow (screenW, screenH);
-    menu ();
+
+    HDC fon_menu = txLoadImage ("Pics\\fon_menu.bmp");
+
+    while (!GetAsyncKeyState(VK_ESCAPE))
+    {
+        drawMenu (screenW, screenH, fon_menu);
+        checkMenuFocus();
+        txSleep(10);
+    }
+
+    txDeleteDC(fon_menu);
 
     return 0;
-    }
+}
 
 
