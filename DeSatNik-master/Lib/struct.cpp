40003cf4e7426687855e1748c1513588a0b16641
Nikitand73 <43246816+Nikitand73@users.noreply.github.com>
2018-10-10 18:03:53 +0400

    Добавил возвращение в меню из планировки

---
+++ DeSatNik-master/Lib/struct.cpp
@@ -0,0 +1,35 @@
+struct Button
+{
+    int x;
+    int y;
+    int x1;
+    int y1;
+    HDC picture;
+    int width;
+    int height;
+};
+
+Button exitButton;
+Button saveButton;
+Button newplanButton;
+Button loadButton;
+Button continueButton;
+
+
+struct furnature
+{
+    int x;
+    int y;
+    int x1;
+    int y1;
+    HDC picture;
+};
+
+
+struct model
+{
+    int sizeX;
+    int sizeY;
+    string title;
+    HDC pic;
+};
---
+++ DeSatNik-master/main.cpp
@@ -0,0 +1,133 @@
+#include "Lib\\TXLib.h"
+#include "Lib\\MENU.cpp"
+
+bool total_exit ();
+void menu_escape(HDC escape);
+
+int count_models = 10;
+
+int main()
+{
+    model Mas_models[count_models];
+    for (int i=0; i<count_models; i++)
+    {
+        Mas_models[i] = {100, 300, "sdf"};
+    }
+
+    int screenW = GetSystemMetrics (SM_CXSCREEN);
+    int screenH = GetSystemMetrics (SM_CYSCREEN);
+    txCreateWindow (screenW, screenH);
+
+    bool startWS = false;
+    bool returnToMenu = false;
+
+    newplanButton = {0, screenH * 80/100, 200, screenH * 85/100};
+    loadButton = {0, screenH * 85/100, 200, screenH * 90/100};
+    saveButton = {0, screenH * 90/100, 200, screenH * 95/100};
+    exitButton = {0, screenH * 95/100, 200, screenH};
+
+    HDC sofa = txLoadImage ("Pics\\Диван.bmp");
+
+
+    Button sofaButton = {100, screenH * 80/100, 100 + screenH * 5/100, screenH * 85/100, sofa, 241, 142};
+    //continueButton = {829, 447, 1065, 483};
+
+
+
+
+    HDC WSpace = txLoadImage ("Pics\\Workspace.bmp");
+    HDC fon_menu = txLoadImage ("Pics\\fon_menu .bmp");
+    HDC escape= txLoadImage ("Pics\\menu_escape.bmp");
+    bool isExit = false;
+
+    while (!isExit)
+    {
+        txBegin();
+
+		//Редактор
+        if (startWS)
+        {
+            Win32::TransparentBlt (txDC(), 0, 0, screenW, screenH, WSpace, 0, 0, 1966, 1104, RGB(123,124,1));
+
+            txSetColor(TX_BLACK);
+            txRectangle(0, screenH - 300, screenW, screenH);
+            menu_escape (escape);
+			ikons (sofaButton);
+			returnToMenu = nazad (returnToMenu);
+			startWS = !returnToMenu;
+
+            txSetColor (TX_BLACK);
+            txLine(127, 955, 1833, 955);
+
+            for (int x = 0; x <= 831; x = x + 30)
+            {
+                txLine   (x, 700, x, 790);
+            }
+        }
+        //Главное меню
+		else
+		{
+			returnToMenu = false;
+            drawMenu (screenW, screenH, fon_menu);
+            checkMenuFocus();
+            //menu_escape(escape);
+            startWS = startWorkspace(startWS);
+            testova(Mas_models, count_models);
+        }
+
+        if (total_exit ())
+        {
+            txDisableAutoPause();
+            isExit = true;
+        };
+
+        txSleep(10);
+        txEnd();
+    }
+
+    txDeleteDC(fon_menu);
+    txDeleteDC(escape);
+    txDeleteDC(WSpace);
+
+    return 0;
+}
+
+bool total_exit ()
+{
+    if (txMouseX() > exitButton.x  &&
+        txMouseX() < exitButton.x1 &&
+        txMouseY() > exitButton.y  &&
+        txMouseY() < exitButton.y1 &&
+        txMouseButtons() & 1)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void menu_escape(HDC escape)
+{
+    int screenW = GetSystemMetrics (SM_CXSCREEN);
+    int screenH = GetSystemMetrics (SM_CYSCREEN);
+
+    bool isreturn = false;
+    if (GetAsyncKeyState(VK_ESCAPE))
+    {
+        txSleep(1000);
+
+        while (!isreturn)
+        {                     //x   y    ГЇВїВЅГЇВїВЅГЇВїВЅ  ГЇВїВЅГЇВїВЅГЇВїВЅ        x    y
+            txBitBlt (txDC(), screenH/2, screenW/2 - 300, 215, 291, escape, 0, 0);
+
+            if ((txMouseButtons() & 1 &&
+                txMouseX() > 0 && txMouseX() < 800
+            &&  txMouseY() > 0 && txMouseY() < 321) or GetAsyncKeyState(VK_ESCAPE))
+            {
+                isreturn = true;
+            }
+
+            txSleep(10);
+        }
+    }
+}
