e3d426a014c6e75fd7d2f48621bd375ef893f329
Devalve <32392126+Devalve@users.noreply.github.com>
2018-10-24 18:04:13 +0400

    короткое создание кнопачек панели инструментов

---
+++ struct.cpp
@@ -0,0 +1,52 @@
+struct Button
+{
+    HDC picture;
+    int x;
+    int y;
+    int x1;
+    int y1;
+    int width;
+    int height;
+};
+
+Button exitButton;
+Button saveButton;
+Button newplanButton;
+Button loadButton;
+Button continueButton;
+Button settingsButton;
+
+
+struct furnature
+{
+    int x;
+    int y;
+    int x1;
+    int y1;
+    HDC picture;
+};
+
+struct model
+{
+    int sizeX;
+    int sizeY;
+    string title;
+    HDC pic;
+};
+
+struct PicSize
+{
+
+    int SizeX;
+    int SizeY;
+};
+
+struct Mebel
+{
+    bool risovat;
+    int MOUSE_X;
+    int MOUSE_Y;
+    HDC pctr;
+};
+
+
